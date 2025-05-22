3e07a829e1c3f3e8dbc75025a4e97ce06983c116
clgoerk <Christopherlee.Goerk@triosstudent.com>
2025-05-22 08:20:52 -0400

    small edit

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
+<?php
+  session_start();
+  require("database.php");
+
+  $queryContacts = 'SELECT * FROM contacts';
+  $statement1 = $db->prepare($queryContacts);
+  $statement1->execute();
+  $contacts = $statement1->fetchAll();
+  $statement1->closeCursor();
+?>
+<!DOCTYPE html>
 <html>
   <head>
     <title>Contact Manager - Home</title>
-    <link rel="stylesheet" type="txt/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
   </head>
   <body>
     <?php include("header.php"); ?>
 
     <main>
       <h2>Contact List</h2>
-      
+
       <table>
         <tr>
           <th>First Name</th>
@@ -18,7 +29,16 @@
           <th>Status</th>
           <th>Birth Date</th>
         </tr>
-
+        <?php foreach ($contacts as $contact): ?>
+        <tr>
+          <td><?php echo $contact['firstName']; ?></td>
+          <td><?php echo $contact['lastName']; ?></td>
+          <td><?php echo $contact['emailAddress']; ?></td>
+          <td><?php echo $contact['phone']; ?></td>
+          <td><?php echo $contact['status']; ?></td>
+          <td><?php echo $contact['dob']; ?></td>
+        </tr>
+        <?php endforeach; ?>
       </table>
     </main>
 
